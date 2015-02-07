051b057efd2ecff0dc2868cba8b10b3ef4d801c4
Parashuram <n.parashuram@yahoo.co.in>
2015-02-07 14:10:11 -0800

    [ui]: Property pages

---
+++ www/app/backend.js
@@ -0,0 +1,23 @@
+angular
+	.module('backend', [])
+	.factory('PropertyData', ['$http', function() {
+		return {
+			getAll: function() {
+				var res = [];
+				for (var i = 0; i < 10; i++) {
+					res.push({
+						id: i,
+						name: 'NAME',
+						desc: 'description',
+						img: 'http://placehold.it/242x200',
+						score: '100'
+					})
+				}
+				return res;
+			},
+
+			getDetails: function() {
+
+			}
+		}
+	}]);
--- www/app/properties/properties.js
+++ www/app/properties/properties.js
@@ -1,11 +1,23 @@
 angular
-	.module('properties', ['ngRoute'])
+	.module('properties', ['ngRoute', 'backend'])
 	.config(['$routeProvider',
 		function($routeProvider) {
 			$routeProvider.when('/properties', {
 				templateUrl: 'app/properties/properties.html',
 				controller: 'PropertiesCtrl',
-				controllerAs: 'properties'
+				controllerAs: 'properties',
+				resolve: {
+					PropertyList: ['PropertyData',
+						function(propertyData) {
+							return propertyData.getAll();
+						}
+					]
+				}
 			});
+		}
+	])
+	.controller('PropertiesCtrl', ['PropertyList',
+		function(list) {
+			this.list = list;
 		}
 	]);
