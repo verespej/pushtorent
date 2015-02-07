9ee50515963f645bc4bc141c6718d94e84959e70
Parashuram <n.parashuram@yahoo.co.in>
2015-02-07 14:24:30 -0800

    [ui]: Added application page

--- www/app/backend.js
+++ www/app/backend.js
@@ -11,13 +11,34 @@ angular
 						desc: 'description',
 						img: 'http://placehold.it/242x200',
 						score: '100'
-					})
+					});
 				}
 				return res;
 			},
 
 			getDetails: function() {
 
+			}
+		}
+	}])
+	.factory('ApplicationData', [function() {
+		return {
+			getAll: function() {
+				var res = [];
+				for (var i = 0; i < 10; i++) {
+					res.push({
+						id: i,
+						property: {
+							id: i,
+							name: 'NAME',
+							desc: 'description',
+							img: 'http://placehold.it/242x200',
+							score: '100'
+						},
+						status: 'underReview'
+					});
+				}
+				return res;
 			}
 		}
 	}]);
