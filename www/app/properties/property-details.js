e2211effbce3acdc66c77024e7577577a063a955
Parashuram <n.parashuram@yahoo.co.in>
2015-02-08 12:58:57 -0800

    asdasd

--- www/app/properties/property-details.js
+++ www/app/properties/property-details.js
@@ -9,12 +9,17 @@ angular
 			});
 		}
 	])
-	.controller('PropertyDetailsCtrl', ['PropertyData', '$routeParams',
-		function(propertyData, routeParams) {
+	.controller('PropertyDetailsCtrl', ['PropertyData', '$routeParams', '$scope', '$location',
+		function(propertyData, routeParams, $scope, $location) {
 			var self = this;
 			propertyData.getDetails(routeParams.id).then(function(data) {
 				console.log(data);
 				self.data = data;
 			});
+
+			$scope.doApply = function() {
+				alert('You application has been sent to review. We will notify you as soon as a descision has been made');
+				$location.path('/home');
+			}
 		}
 	]);
